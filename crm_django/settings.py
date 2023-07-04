c43bc40de7b731e0823b4fff17fca5cc2eddb71d
Roger Reyes <kadoshrr2019@gmail.com>
2023-07-03 19:04:37 -0500

    Installed dotenv, Maked file .env, Implementation dotenv

--- crm_django/settings.py
+++ crm_django/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx2sqfv=-6vf5y6+jir07oqq%*575#4ogi#oyi575c2j61$wcp'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
