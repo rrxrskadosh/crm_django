69a61549089db4555eb2951766b2931afa618323
Roger Reyes <kadoshrr2019@gmail.com>
2023-07-24 00:25:50 -0500

    Imported Path in settings and Maked directory Templates, Modifying views leads adding context to second page, Modifying Home page, Building Second page

--- crm_django/settings.py
+++ crm_django/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +48,7 @@ ROOT_URLCONF = 'crm_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
